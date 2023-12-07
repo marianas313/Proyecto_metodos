e76035da826defe9c66fa20f4b6e62e43d2b1d44
Mariana Tafur <marianas.tafurh@autonoma.edu.co>
2023-12-07 15:01:29 -0500

    segunda version

--- pages/14 Reglas de Simpson.py
+++ pages/14 Reglas de Simpson.py
@@ -5,80 +5,115 @@ import sympy as sp
 import pandas as pd
 
 st.markdown('## Reglas de Simpson')
+# Entrada de los límites y la función
+a = st.number_input("Ingrese el límite inferior a:", value=1)
+b = st.number_input("Ingrese el límite superior b:", value=6)
+fx_input = st.text_input("Ingrese la función f(x):", "x * sin(x) + 3 * x - 1")
 tabs= st.tabs(['Simpson 1/3','Simpson 3/8'])
 with tabs[0]:
-    def simpsons_rule(a, b, n, func):
-        x = sp.symbols('x')
-        fx = sp.sympify(func)
-
-        h = (b - a) / n
-        odd_terms = [4 * fx.subs(x, a + i * h) for i in range(1, n, 2)]
-        even_terms = [2 * fx.subs(x, a + i * h) for i in range(2, n - 1, 2)]
-
-        I = (h / 3) * (fx.subs(x, a) + sum(odd_terms) + sum(even_terms) + fx.subs(x, b))
-
-        return I
 
     # Interfaz de usuario con Streamlit
     st.title("Regla de Simpson 1/3")
 
-    # Entrada de los límites y número de intervalos
-    a = st.number_input("Ingrese el límite inferior a:", value=1)
-    b = st.number_input("Ingrese el límite superior b:", value=2)
-    n = st.number_input("Ingrese el número de intervalos n:", value=4, step=2)
+    # Definir la variable simbólica x y la función simbólica fx
+    x = sp.symbols('x')
+    fx = sp.sympify(fx_input)
 
-    # Entrada de la función
-    func = st.text_input("Ingrese la función f(x):", "x**2")
+    # Evaluar la función en a, b y m
+    fa = fx.subs(x, a)
+    fb = fx.subs(x, b)
+    m = (a + b) / 2
+    fm = fx.subs(x, m)
 
-    # Calcular la integral con la regla de Simpson 1/3
-    try:
-        I_approx = simpsons_rule(a, b, n, func)
-        st.write(f"La integral aproximada es: {I_approx}")
+    st.write(f"f(a) = {fa} \nf(b) = {fb} \nf(m) = {fm:.2f}")
 
-        # Calcular la integral real usando sympy
-        x = sp.symbols('x')
-        fx = sp.sympify(func)
-        I_real = sp.integrate(fx, (x, a, b))
-        st.write(f"La integral real por sympy es: {I_real}")
+    # Aplicar la regla de Simpson 1/3
+    I = (b - a) * ((fa + 4 * fm + fb) / 6)
+    st.write(f"I = {I} U^2")
 
-        # Calcular el error relativo
-        error = abs((I_real - I_approx) / I_real) * 100
-        st.write(f"El error relativo es: {error:.2f}%")
+    # Calcular la integral real usando sympy
+    I_real = sp.integrate(fx, (x, a, b))
+    st.write("La integral por sympy es:", I_real)
 
-        # Crear gráfica
-        x_values = np.linspace(a, b, 100)
-        y_values = [fx.subs(x, val) for val in x_values]
+    # Calcular el error relativo
+    error = abs((I_real - I) / I_real) * 100
+    st.write(f"El error relativo es: {error:.2f}%")
 
-        fig, ax = plt.subplots()
-        ax.plot(x_values, y_values, color='blue', label='f(x)')
-        ax.grid()
+    # Crear gráfica
+    x_values = np.linspace(0, b, 100)
+    y_values = [fx.subs(x, val) for val in x_values]
 
-        # Plano cartesiano (Ejes)
-        ax.vlines(x=0, ymin=0, ymax=max(y_values), color='k')
-        ax.hlines(y=0, xmin=a, xmax=b, color='k')
+    fig, ax = plt.subplots()
+    ax.plot(x_values, y_values, color='blue', label='f(x)')
+    ax.grid()
 
-        # Límites xl y xu
-        ax.vlines(x=a, ymin=0, ymax=fx.subs(x, a), color='r', linestyle='--')
-        ax.vlines(x=b, ymin=0, ymax=fx.subs(x, b), color='r', linestyle='--')
+    # Plano cartesiano (Ejes)
+    ax.vlines(x=0, ymin=0, ymax=max(y_values), color='k')
+    ax.hlines(y=0, xmin=0, xmax=b, color='k')
 
-        # Área bajo la curva
-        x_fill = np.linspace(a, b, 100)
-        y_fill = [fx.subs(x, val) for val in x_fill]
-        ax.fill_between(x_fill, y_fill, color='r', alpha=0.2, label='Área bajo la curva')
+    # Límites xl y xu
+    ax.vlines(x=a, ymin=0, ymax=fa, color='r', linestyle='--')
+    ax.vlines(x=b, ymin=0, ymax=fb, color='r', linestyle='--')
 
-        plt.grid(True)
-        plt.legend()
+    ax.fill([a, a, m, b, b], [0, fa, fm, fb, 0], 'r', alpha=0.2)
 
-        # Mostrar gráfica
-        st.pyplot(fig)
+    plt.grid(True)
+    plt.legend()
 
-    except Exception as e:
-        st.write(f"Error: {e}")
+    # Mostrar gráfica en Streamlit
+    st.pyplot(fig)
+
 
 
 with tabs[1]:
-    st.markdown("Simpson 3/8")
-    st.image('https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhrXcP6z77LAuWpyCEFfjd102XshQiVVjM1uTm1Of6rDn5tS9G9bg90yslI_q009LvdKvD6jZPVwB4u2ItIc1GpYaZacKes9pmhJpIEv-hhvymHXOtLhQupPeyYMQRiG2VgjCLMnEAmz60-o4KPeEW5LbA_DDSjsw24jn7twugTRv9VLwCGpCnzfeyR/s953/SIMPLEX.png',width=500)
-    #codigo aqui
 
-    #codigo aqui
+    # Función para la regla de Simpson 3/8
+    def simpsons_3_8_rule(a, b, n, func):
+        x = sp.symbols('x')
+        fx = sp.sympify(func)
+
+        h = (b - a) / n
+        terms = [3 * fx.subs(x, a + i * h) for i in range(1, n, 3)]
+        terms += [3 * fx.subs(x, a + i * h) for i in range(2, n - 2, 3)]
+        terms += [2 * fx.subs(x, a + i * h) for i in range(3, n - 3, 3)]
+
+        I = (3 * h / 8) * (fx.subs(x, a) + sum(terms) + fx.subs(x, b))
+
+        return I
+
+    # Interfaz de usuario con Streamlit
+    st.title("Regla de Simpson 3/8")
+
+    # Entrada del número de segmentos
+    n = st.number_input("Ingrese el número de segmentos n:", value=4, step=3)
+
+    # Calcular la integral con la regla de Simpson 3/8
+    I_approx = simpsons_3_8_rule(a, b, n, fx_input)
+    st.write(f"La integral aproximada es: {I_approx}")
+
+    # Crear gráfica
+    x_values = np.linspace(a, b, 100)
+    y_values = [sp.lambdify('x', fx_input)(val) for val in x_values]
+
+    fig, ax = plt.subplots()
+    ax.plot(x_values, y_values, color='blue', label='f(x)')
+    ax.grid()
+
+    # Plano cartesiano (Ejes)
+    ax.vlines(x=0, ymin=0, ymax=max(y_values), color='k')
+    ax.hlines(y=0, xmin=a, xmax=b, color='k')
+
+    # Límites xl y xu
+    ax.vlines(x=a, ymin=0, ymax=sp.lambdify('x', fx_input)(a), color='r', linestyle='--')
+    ax.vlines(x=b, ymin=0, ymax=sp.lambdify('x', fx_input)(b), color='r', linestyle='--')
+
+    # Área bajo la curva
+    x_fill = np.linspace(a, b, 100)
+    y_fill = [sp.lambdify('x', fx_input)(val) for val in x_fill]
+    ax.fill_between(x_fill, y_fill, color='r', alpha=0.2, label='Área bajo la curva')
+
+    plt.grid(True)
+    plt.legend()
+
+    # Mostrar gráfica en Streamlit
+    st.pyplot(fig)
